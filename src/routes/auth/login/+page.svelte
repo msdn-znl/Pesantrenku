5d1c8a1a01b45161867a9a8af08826081faa6306
Muhammad Sirojuddin <nidu672@gmail.com>
2025-07-14 15:10:26 +0700

    feature: basic login/logout

---
+++ src/routes/auth/login/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import type { ActionData } from './$types';
+
+	let { form }: { form: ActionData } = $props();
+</script>
+
+<h1>Login/Register</h1>
+<form method="post" action="?/login" use:enhance>
+	<label>
+		Username
+		<input name="username" type="email" class="input" />
+	</label>
+	<label>
+		Password
+		<input type="password" name="password" class="input" />
+	</label>
+	<button class="btn btn-success hover:btn-accent">Login</button>
+</form>
+<p style="color: red">{form?.message ?? ''}</p>
