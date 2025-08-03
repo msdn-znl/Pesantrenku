358fb51c99adbfaaa530ab4bfc0b43a2bd7271a9
Muhammad Sirojuddin <nidu672@gmail.com>
2025-08-03 09:42:54 +0700

    feature: modal dialog for creating and delete confirmation in /periode

--- src/routes/(authed)/periode/+page.svelte
+++ src/routes/(authed)/periode/+page.svelte
@@ -3,27 +3,55 @@
 	import { enhance } from '$app/forms';
 
 	let { data, form }: { data: PageServerData; form: ActionData } = $props();
+	let createPeriodeModal: HTMLDialogElement;
+	let deletePeriodeModal: HTMLDialogElement;
+	let periodeToDelete = $state<number | null>(null);
 </script>
 
-<div class="card max-w-md">
-	<h2 class="card-title">Tambah Kitab</h2>
-	<div class="card-body">
-		<form action="?/create" method="post" use:enhance autocomplete="off" class="flex flex-col">
-			<fieldset class="fieldset">
-				<label for="tahunAjaran">Periode</label>
-				<input
-					type="text"
-					name="tahunAjaran"
-					id="tahunAjaran"
-					class="input"
-					placeholder="Contoh: 2025/2026 Genap"
-					required
-				/>
-				<button type="submit" class="btn btn-success">Tambah Data</button>
-			</fieldset>
-		</form>
-		<p>{form?.message ?? ''}</p>
+<div class="">
+	<dialog class="modal" id="create_periode_modal" bind:this={createPeriodeModal}>
+		<div class="modal-box">
+			<form method="dialog">
+				<button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+			</form>
+			<div>
+				<form action="?/create" method="post" use:enhance autocomplete="off" class="flex flex-col">
+					<fieldset class="fieldset">
+						<label for="tahunAjaran">Periode</label>
+						<input
+							type="text"
+							name="tahunAjaran"
+							id="tahunAjaran"
+							class="input"
+							placeholder="Contoh: 2025/2026 Genap"
+							required
+						/>
+						<button type="submit" class="btn btn-success">Tambah Data</button>
+					</fieldset>
+				</form>
+			</div>
+			<p>{form?.message ?? ''}</p>
+		</div>
+	</dialog>
+	<div class="flex flex-row-reverse p-2">
+		<button class="btn btn-success mr-2" onclick={() => createPeriodeModal.showModal()}
+			>Tambah Periode</button
+		>
 	</div>
+	<dialog class="modal" id="delete_periode_modal" bind:this={deletePeriodeModal}>
+		<div class="modal-box">
+			<p>Apakah Anda yakin ingin menghapus data periode ini?</p>
+			<div class="flex flex-row">
+				<form method="dialog">
+					<button class="btn btn-success" onclick={() => (periodeToDelete = null)}>Batal</button>
+				</form>
+				<form action="?/delete" method="post" use:enhance>
+					<input type="hidden" name="id" value={periodeToDelete} />
+					<button type="submit" class="btn btn-error">Delete</button>
+				</form>
+			</div>
+		</div>
+	</dialog>
 </div>
 
 <div class="card">
@@ -42,12 +70,15 @@
 						<th>{i + 1}</th>
 						<td>{periode.tahunAjaran}</td>
 						<td>
-							<div>
-								<form action="?/delete" method="post" use:enhance>
-									<input type="hidden" name="id" value={periode.id} />
-									<button type="submit" class="btn btn-error">Delete</button>
-								</form>
-							</div>
+							<button
+								class="btn btn-error"
+								onclick={() => {
+									periodeToDelete = periode.id;
+									deletePeriodeModal.showModal();
+								}}
+							>
+								Delete
+							</button>
 						</td>
 					</tr>
 				{/each}
