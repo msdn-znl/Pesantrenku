69e8ca9554f001bb7814010720c435e1d67fe264
Muhammad Sirojuddin <nidu672@gmail.com>
2025-08-06 19:42:36 +0700

    feature: add styling to landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="hero bg-base-200 min-h-screen">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+			<h1 class="text-5xl font-bold">SELAMAT DATANG</h1>
+			<p class="py-6">
+				Selamat datang di Pesantrenku, sebuah portal untuk manajemen santri di Pondok Pesantren
+				Darussalam Keputih
+			</p>
+			<a href="/register">
+				<button class="btn btn-primary">Ayo Mulai</button>
+			</a>
+		</div>
+	</div>
+</div>
