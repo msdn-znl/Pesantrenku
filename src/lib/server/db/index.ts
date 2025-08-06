2c1b0be8c63d8cb5558d2317c7b5de0fbe615d89
Muhammad Sirojuddin <nidu672@gmail.com>
2025-08-06 19:44:04 +0700

    chore: remove unused dotenv package

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,15 +1,15 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import * as schema from './schema';
-// import { env } from '$env/dynamic/private';
-import { config } from 'dotenv';
+import { env } from '$env/dynamic/private';
+// import { config } from 'dotenv';
 
-config({ path: '/home/msdn/Pesantrenku/.env' });
+// config({ path: '/home/msdn/Pesantrenku/.env' });
 
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 const pool = new Pool({
-	connectionString: process.env.DATABASE_URL
+	connectionString: env.DATABASE_URL
 });
 
 export const db = drizzle({ client: pool, schema: schema });
