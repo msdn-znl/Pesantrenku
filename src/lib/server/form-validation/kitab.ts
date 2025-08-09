9e3d43bfe8c003cfcfaba410a5feab253208baad
Muhammad Sirojuddin <nidu672@gmail.com>
2025-08-10 01:05:16 +0700

    feature: add modal for create, delete, and edit kitab

--- src/lib/server/form-validation/kitab.ts
+++ src/lib/server/form-validation/kitab.ts
@@ -6,4 +6,11 @@ export const KitabFormSchema = z.object({
 	kategori: z.string().max(20).optional()
 });
 
+export const EditKitabFormSchema = z.object({
+	id: z.coerce.number(),
+	namaKitab: z.string().max(100),
+	pengarang: z.string().max(255).optional(),
+	kategori: z.string().max(20).optional()
+});
+
 export type KitabForm = z.infer<typeof KitabFormSchema>;
--- src/routes/(authed)/kitab/+page.svelte
+++ src/routes/(authed)/kitab/+page.svelte
@@ -1,53 +1,174 @@
 <script lang="ts">
 	import type { PageServerData, ActionData } from './$types';
-	import { enhance } from '$app/forms';
+	import { enhance, applyAction } from '$app/forms';
+	import { invalidateAll } from '$app/navigation';
+	import { toast } from 'svelte-sonner';
+	type Kitab = PageServerData['kitabList'][number];
 
 	let { data, form }: { data: PageServerData; form: ActionData } = $props();
 
 	let createKitabModal: HTMLDialogElement;
 	let deleteKitabModal: HTMLDialogElement;
+	let editKitabModal: HTMLDialogElement;
 	let kitabToDelete = $state<number | null>(null);
+	let kitabToEdit = $state<Kitab | null>(null);
 </script>
 
 <div class="">
 	<dialog class="modal" id="create_kitab_modal" bind:this={createKitabModal}>
-		<div class="card bg-base-100">
+		<div class="modal-box">
 			<form method="dialog">
 				<button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
 			</form>
-			<div class="card-body">
-				<h2 class="card-title">Tambah Kitab</h2>
-				<form action="?/create" method="post" use:enhance autocomplete="off" class="flex flex-col">
-					<fieldset class="fieldset">
-						<label for="namaKitab" class="label">Nama Kitab</label>
-						<input
-							type="text"
-							name="namaKitab"
-							id="namaKitab"
-							class="input w-xs lg:w-lg"
-							placeholder="Contoh: Muttamimmah"
-						/>
-						<label for="pengarang" class="label">Pengarang</label>
-						<input
-							type="text"
-							name="pengarang"
-							id="pengarang"
-							class="input w-xs lg:w-lg"
-							placeholder="Contoh: Syekh Nawawi"
-						/>
-						<label for="kategori" class="label">Kategori</label>
-						<input
-							type="text"
-							name="kategori"
-							id="kategori"
-							class="input w-xs lg:w-lg"
-							placeholder="Contoh: Fiqih"
-						/>
-						<button type="submit" class="btn btn-success mt-5">Tambah Data</button>
-					</fieldset>
+			<h2 class="card-title">Tambah Kitab</h2>
+			<form
+				action="?/create"
+				method="post"
+				autocomplete="off"
+				use:enhance={() => {
+					return async ({ result }) => {
+						if (result.type === 'success') {
+							invalidateAll();
+							createKitabModal.close();
+							if (result.data?.message && typeof result.data?.message === 'string') {
+								toast.success(result.data?.message);
+							}
+						} else if (result.type === 'failure') {
+							createKitabModal.close();
+							if (result.data?.message && typeof result.data.message === 'string') {
+								toast.error(result.data?.message);
+							}
+						}
+						await applyAction(result);
+					};
+				}}
+			>
+				<fieldset class="fieldset">
+					<label for="namaKitab" class="label">Nama Kitab</label>
+					<input
+						type="text"
+						name="namaKitab"
+						id="namaKitab"
+						class="input w-full"
+						placeholder="Contoh: Muttamimmah"
+					/>
+					<label for="pengarang" class="label">Pengarang</label>
+					<input
+						type="text"
+						name="pengarang"
+						id="pengarang"
+						class="input w-full"
+						placeholder="Contoh: Syekh Nawawi"
+					/>
+					<label for="kategori" class="label">Kategori</label>
+					<input
+						type="text"
+						name="kategori"
+						id="kategori"
+						class="input w-full"
+						placeholder="Contoh: Fiqih"
+					/>
+					<button type="submit" class="btn btn-success mt-5">Tambah Data</button>
+				</fieldset>
+			</form>
+		</div>
+	</dialog>
+	<dialog class="modal" id="delete_user_modal" bind:this={deleteKitabModal}>
+		<div class="modal-box">
+			<p>Apakah Anda yakin ingin menghapus data ini?</p>
+			<div class="modal-action">
+				<form method="dialog">
+					<button class="btn btn-success" onclick={() => (kitabToDelete = null)}>Batal</button>
 				</form>
-				<p>{form?.message ?? ''}</p>
+				<form
+					action="?/delete"
+					method="post"
+					use:enhance={() => {
+						return async ({ result }) => {
+							if (result.type === 'success') {
+								invalidateAll();
+								deleteKitabModal.close();
+								if (result.data?.message && typeof result.data?.message === 'string') {
+									toast.success(result.data?.message);
+								}
+							} else if (result.type === 'failure') {
+								deleteKitabModal.close();
+								if (result.data?.message && typeof result.data.message === 'string') {
+									toast.error(result.data?.message);
+								}
+							}
+							await applyAction(result);
+						};
+					}}
+				>
+					<input type="hidden" name="id" value={kitabToDelete} />
+					<button type="submit" class="btn btn-error">Delete</button>
+				</form>
 			</div>
+		</div>
+	</dialog>
+	<dialog class="modal" id="edit_user_modal" bind:this={editKitabModal}>
+		<div class="modal-box">
+			<h2 class="card-title">Edit Kitab</h2>
+			<form method="dialog">
+				<button
+					class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+					onclick={() => (kitabToEdit = null)}>✕</button
+				>
+			</form>
+			<form
+				action="?/edit"
+				method="post"
+				class=""
+				use:enhance={() => {
+					return async ({ result }) => {
+						if (result.type === 'success') {
+							invalidateAll();
+							editKitabModal.close();
+							if (result.data?.message && typeof result.data?.message === 'string') {
+								toast.success(result.data?.message);
+							}
+						} else if (result.type === 'failure') {
+							editKitabModal.close();
+							if (result.data?.message && typeof result.data.message === 'string') {
+								toast.error(result.data?.message);
+							}
+						}
+						await applyAction(result);
+					};
+				}}
+			>
+				<fieldset class="fieldset">
+					<input type="number" name="id" id="id" value={kitabToEdit?.id} hidden />
+					<label class="label" for="namaKitab">Nama Kitab</label>
+					<input
+						type="text"
+						name="namaKitab"
+						id="namaKitab"
+						class="input w-full"
+						value={kitabToEdit?.namaKitab}
+					/>
+					<label for="pengarang">Pengarang</label>
+					<input
+						type="text"
+						name="pengarang"
+						id="pengarang"
+						class="input w-full"
+						value={kitabToEdit?.pengarang}
+						placeholder="Contoh: Imam Nawawi"
+					/>
+					<label for="kategori">Kategori</label>
+					<input
+						type="text"
+						name="kategori"
+						id="kategori"
+						class="input w-full"
+						placeholder="Contoh: Fiqih"
+						value={kitabToEdit?.kategori}
+					/>
+				</fieldset>
+				<button type="submit" class="btn btn-success mt-4">Edit Data</button>
+			</form>
 		</div>
 	</dialog>
 	<div class="flex flex-row-reverse p-2">
@@ -55,20 +176,6 @@
 			>Tambah Kitab</button
 		>
 	</div>
-	<dialog class="modal" id="delete_user_modal" bind:this={deleteKitabModal}>
-		<div class="modal-box">
-			<p>Apakah Anda yakin ingin menghapus data ini?</p>
-			<div class="flex flex-row">
-				<form method="dialog">
-					<button class="btn btn-success" onclick={() => (kitabToDelete = null)}>Batal</button>
-				</form>
-				<form action="?/delete" method="post" use:enhance>
-					<input type="hidden" name="id" value={kitabToDelete} />
-					<button type="submit" class="btn btn-error">Delete</button>
-				</form>
-			</div>
-		</div>
-	</dialog>
 	<div class="p-2">
 		<h1 class="card-title">List Kitab</h1>
 		<div class="overflow-auto">
@@ -88,9 +195,13 @@
 							<td>{kitab.namaKitab}</td>
 							<td>{kitab.pengarang}</td>
 							<td>{kitab.kategori}</td>
-							<td>
-								<a href={'/kitab/edit-data/' + kitab.id} class="btn btn-accent"
-									><button>Edit</button></a
+							<td class="flex flex-col">
+								<button
+									class="btn btn-accent"
+									onclick={() => {
+										kitabToEdit = { ...kitab };
+										editKitabModal.showModal();
+									}}>Edit</button
 								>
 								<button
 									class="btn btn-error"
