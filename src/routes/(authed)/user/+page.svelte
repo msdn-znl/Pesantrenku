680f2148a93b6023be27c0bb6c766997d5636e31
Muhammad Sirojuddin <nidu672@gmail.com>
2025-08-10 01:19:10 +0700

    fix: data type for edit user

--- src/routes/(authed)/user/+page.svelte
+++ src/routes/(authed)/user/+page.svelte
@@ -13,7 +13,7 @@
 	let editUserForm: HTMLFormElement;
 	let editUserModal: HTMLDialogElement;
 	let userToDelete = $state<string | null>(null);
-	let userToEdit = $state<User | undefined>();
+	let userToEdit = $state<User | null>(null);
 </script>
 
 <div class="">
@@ -117,7 +117,7 @@
 			<form method="dialog">
 				<button
 					class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-					onclick={() => (userToEdit = undefined)}>✕</button
+					onclick={() => (userToEdit = null)}>✕</button
 				>
 			</form>
 			<form
