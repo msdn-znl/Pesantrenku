f2ce87dc76636e416806c539d3e2b8fe3e3c5b78
Muhammad Sirojuddin <nidu672@gmail.com>
2025-09-03 23:18:35 +0700

    vercel: speed insight

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,7 @@
 import type { LayoutServerLoad } from './$types';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
 	return { user: locals.user };
 };
+injectSpeedInsights();
