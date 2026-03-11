181a618bab174ab0a65e4bf564e43f9058fdb82a
Muhammad Sirojuddin <nidu672@gmail.com>
2026-03-11 19:30:36 +0700

    feat(user validation): define different data to process based on role to enable bulk user insert

--- src/lib/server/form-validation/user.ts
+++ src/lib/server/form-validation/user.ts
@@ -1,11 +1,26 @@
 import * as z from 'zod/v4';
 
-export const UserFormSchema = z.object({
+export const UserAdminFormSchema = z.object({
+	role: z.literal('admin'),
 	username: z.string().nonempty(),
 	password: z.string().min(6).max(255),
-	nama: z.string(),
-	role: z.enum(['admin', 'santri', 'guru'])
+	nama: z.string()
 });
+export const UserSantriFormSchema = z.object({
+	role: z.literal('santri'),
+	nama: z.string(),
+	tipe: z.enum(['putra', 'putri'])
+});
+
+export const UserGuruFormSchema = z.object({
+	role: z.literal('guru'),
+	nama: z.string()
+});
+export const CreateUserSchema = z.discriminatedUnion('role', [
+	UserAdminFormSchema,
+	UserSantriFormSchema,
+	UserGuruFormSchema
+]);
 
 export const LoginUserFormSchema = z.object({
 	username: z.string().nonempty(),
@@ -25,3 +40,10 @@ export const RegisterFormSchema = z.object({
 	password: z.string().min(6).max(255),
 	nama: z.string()
 });
+
+export const DeleteUserSchema = z.object({
+	id: z.array(z.string())
+});
+
+export type CreateUserForm = z.infer<typeof CreateUserSchema>;
+export type RoleUser = CreateUserForm['role'];
