db062bdf4a87693f49b9c272299ac4ee4760cfd5
Muhammad Sirojuddin <nidu672@gmail.com>
2026-03-11 19:11:43 +0700

    chore(utils): add helper function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,10 +2,56 @@ import { customAlphabet } from 'nanoid';
 const alphabet = '0123456789abcdefghijklmnopqrstuvwxyz';
 export const generateId = customAlphabet(alphabet, 15);
 
+import type { Jadwal } from './server/db/schema';
+
 export const getHariIni = () => {
 	const days = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'];
 	const d = new Date();
 	return days[d.getDay()];
 };
+export const getTanggalSekarang = () => {
+	const d = new Date().toDateString();
+	return d;
+};
+
+export const getTahunSekarang = () => {
+	const d = new Date();
+	return d.getFullYear();
+};
 
 export type dayString = 'Minggu' | 'Senin' | 'Selasa' | 'Rabu' | 'Kamis' | 'Jumat' | 'Sabtu';
+
+export const groupJadwal = (listJadwal: Array<Jadwal>) => {
+	const container = new Map();
+	listJadwal.forEach((item) => {
+		const uniqueKey = JSON.stringify({
+			kitab: item.kitabId,
+			kelas: item.kelasId,
+			guru: item.guruId,
+			jamMulai: item.jamMulai,
+			jamSelesai: item.jamSelesai
+		});
+		if (container.has(uniqueKey)) {
+			const existingItem = container.get(uniqueKey);
+			if (!existingItem.hari.includes(item.hari)) {
+				existingItem.hari.push(item.hari);
+			}
+		} else {
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+			const { id, hari, ...rest } = item;
+			const newItem = {
+				...rest,
+				hari: [hari]
+			};
+			container.set(uniqueKey, newItem);
+		}
+	});
+	return Array.from(container.values());
+};
+
+export const generateNIS = (tipe: string, tahun: number, urutan: string) => {
+	const prefix = tipe == 'putra' ? '10' : '20';
+	const postfix = urutan.padStart(3, '0');
+	const finalNIS = prefix + tahun.toString() + postfix;
+	return finalNIS;
+};
