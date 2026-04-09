3217d48fd164415e1a139e5c2287551836798865
Muhammad Sirojuddin <nidu672@gmail.com>
2026-04-09 23:23:28 +0700

    refactor: mengubah field form untuk menyesuaikan dengan database baru

--- src/routes/(authed)/(admin)/periode/+page.svelte
+++ src/routes/(authed)/(admin)/periode/+page.svelte
@@ -1,21 +1,53 @@
 <script lang="ts">
-	import type { PageServerData, ActionData } from './$types';
+	import type { PageServerData } from './$types';
 	import { enhance, applyAction } from '$app/forms';
 	import { invalidateAll } from '$app/navigation';
 	import { toast } from 'svelte-sonner';
 
-	let { data, form }: { data: PageServerData; form: ActionData } = $props();
+	let { data }: { data: PageServerData } = $props();
 	let createPeriodeModal: HTMLDialogElement;
 	let deletePeriodeModal: HTMLDialogElement;
+	let activatePeriodeModal: HTMLDialogElement;
 	let periodeToDelete = $state<string | null>(null);
+	let periodeToActivate = $state<string | null>(null);
 </script>
 
 <svelte:head>
 	<title>Menu Tahun Ajar</title>
 </svelte:head>
 <div>
+	<dialog
+		class="modal"
+		id="activate_periode_modal"
+		bind:this={activatePeriodeModal}
+		onclose={() => {
+			periodeToActivate = null;
+		}}
+	>
+		<div class="modal-box">
+			<form method="dialog">
+				<button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+			</form>
+			<div>
+				<form action="?/activate" method="post">
+					<input
+						type="text"
+						name="tahunAjaranId"
+						id="tahunAjaranId"
+						value={periodeToActivate}
+						hidden
+					/>
+					<p>Apakah Anda Yakin Untuk Mengaktifkan Periode ini?</p>
+					<div class="modal-action">
+						<button type="submit" class="btn btn-success">Ya</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</dialog>
 	<dialog class="modal" id="create_periode_modal" bind:this={createPeriodeModal}>
 		<div class="modal-box">
+			<h2 class="card-title">Tambah Periode</h2>
 			<form method="dialog">
 				<button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
 			</form>
@@ -43,15 +75,31 @@
 					}}
 				>
 					<fieldset class="fieldset">
-						<label for="tahunAjaran">Periode</label>
+						<label for="tahunMulai" class="label">Tahun Mulai</label>
 						<input
-							type="text"
-							name="tahunAjaran"
-							id="tahunAjaran"
-							class="input"
-							placeholder="Contoh: 2025/2026 Genap"
+							type="number"
+							name="tahunMulai"
+							id="tahunMulai"
+							class="input w-full"
+							placeholder="Contoh: 2025"
 							required
 						/>
+						<label for="tahunSelesai" class="label">Tahun Mulai</label>
+						<input
+							type="number"
+							name="tahunSelesai"
+							id="tahunSelesai"
+							class="input w-full"
+							placeholder="Contoh: 2026"
+							required
+						/>
+						<label for="tipeSemester" class="label">Semester</label>
+						<select name="tipeSemester" id="tipeSemester" class="select w-full">
+							<option value=""></option>
+							<option value="ganjil">Ganjil</option>
+							<option value="genap">Genap</option>
+						</select>
+
 						<button type="submit" class="btn btn-success">Tambah Data</button>
 					</fieldset>
 				</form>
@@ -107,14 +155,39 @@
 				<tr>
 					<th></th>
 					<th>Periode</th>
+					<th>Status</th>
+					<th>Action</th>
 				</tr>
 			</thead>
 			<tbody>
 				{#each data.periodeList as periode, i (periode.id)}
-					<tr>
+					<tr class="">
 						<th>{i + 1}</th>
-						<td>{periode.tahunAjaran}</td>
+						<td
+							>{periode.tahunMulai +
+								'/' +
+								periode.tahunSelesai +
+								' ' +
+								periode.tipeSemester?.toUpperCase()}</td
+						>
 						<td>
+							{#if periode.isActive === true}
+								<div class="badge badge-soft badge-success">Aktif</div>
+							{:else}<div class="badge badge-soft badge-error">Tidak Aktif</div>{/if}
+						</td>
+						<td>
+							{#if periode.isActive === false}
+								<button
+									name="periodeNonAktif"
+									class="btn btn-success"
+									onclick={() => {
+										periodeToActivate = periode.id;
+										activatePeriodeModal.showModal();
+									}}>Aktifkan</button
+								>
+							{:else if periode.isActive === true}
+								<button name="periodeAktif" class="btn btn-success">Aktif</button>
+							{/if}
 							<button
 								class="btn btn-error"
 								onclick={() => {
