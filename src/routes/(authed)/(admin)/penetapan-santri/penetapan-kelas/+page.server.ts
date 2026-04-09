9c0ce818aa0352fbfd7cdc08592f16b7a07488e3
Muhammad Sirojuddin <nidu672@gmail.com>
2026-04-10 00:25:03 +0700

    feat: menambah halaman penetapan kelas santri(load dan server action saja)

---
+++ src/routes/(authed)/(admin)/penetapan-santri/penetapan-kelas/+page.server.ts
@@ -0,0 +1,77 @@
+import type { PageServerLoad, Actions, RequestEvent } from './$types';
+import { db } from '$lib/server/db';
+import * as table from '$lib/server/db/schema';
+import { sql } from 'drizzle-orm';
+import { fail } from '@sveltejs/kit';
+import { KelasSantriFormSchema } from '$lib/server/form-validation/kelas_santri';
+import { generateId } from '$lib/utils';
+
+export const load: PageServerLoad = async () => {
+	try {
+		const daftarSantri = await db.query.santri.findMany({
+			columns: { id: true, userId: true, nomorIndukSantri: true },
+			with: {
+				user: { columns: { name: true } },
+				kelas_santri: { with: { kelas: { columns: { namaKelas: true } } } }
+			},
+			where: (santriTable, { exists, and, eq }) =>
+				exists(
+					db
+						.select({ _: sql`1` })
+						.from(table.pendaftaran_santri)
+						.innerJoin(
+							table.tahun_ajaran,
+							eq(table.pendaftaran_santri.tahunAjaranId, table.tahun_ajaran.id)
+						)
+						.where(
+							and(
+								eq(table.pendaftaran_santri.santriId, santriTable.id),
+								eq(table.tahun_ajaran.isActive, true)
+							)
+						)
+				)
+		});
+		return { daftarSantri };
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+export const actions: Actions = {
+	tambahKelas: async (event: RequestEvent) => {
+		const formData = await event.request.formData();
+		const data = Object.fromEntries(
+			Array.from(formData.keys()).map((key) => [
+				key,
+				formData.getAll(key).length > 1 ? formData.getAll(key) : formData.get(key)
+			])
+		);
+		const validation = KelasSantriFormSchema.safeParse(data);
+		if (!validation.success) {
+			return fail(422, {
+				message: 'Data yang anda masukkan salah',
+				data: validation.data
+			});
+		}
+		const id = validation.data.idKelas;
+		if (!id) {
+			return fail(422, {
+				message: 'id kelas tidak ada'
+			});
+		}
+		const insertData: (typeof table.kelas_santri.$inferInsert)[] = validation.data.idSantri.map(
+			(item) => ({
+				id: generateId(),
+				santriId: item,
+				kelasId: id
+			})
+		);
+		try {
+			await db.insert(table.kelas_santri).values(insertData);
+			return { success: true };
+		} catch (err) {
+			console.error(`Terjadi Error:`, err);
+			return fail(500, { message: ' terjadi kesalahan di Server saat input data' });
+		}
+	}
+};
---
+++ src/routes/(authed)/(admin)/penetapan-santri/penetapan-kelas/+page.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+	import type { PageServerData, ActionData } from './$types';
+	let { data, form }: { data: PageServerData; form: ActionData } = $props();
+</script>
