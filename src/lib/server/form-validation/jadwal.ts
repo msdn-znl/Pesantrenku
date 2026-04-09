a1c81da2a71f740c8c0c4b53b80ac29951c22c92
Muhammad Sirojuddin <nidu672@gmail.com>
2026-04-09 20:48:30 +0700

    feat: tambah validasi untuk 2 tipe data

--- src/lib/server/form-validation/jadwal.ts
+++ src/lib/server/form-validation/jadwal.ts
@@ -4,7 +4,12 @@ export const JadwalFormSchema = z.object({
 	kitabId: z.string(),
 	kelasId: z.string(),
 	guruId: z.string(),
-	hari: z.array(z.enum(['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu'])),
+	hari: z.union([
+		z.enum(['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu']).transform((val) => {
+			return [val];
+		}),
+		z.array(z.enum(['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu']))
+	]),
 	jamMulai: z.string().min(1, 'jam mulai wajib diisi'),
 	jamSelesai: z.string().min(1, 'jam selesai wajib diisi')
 });
