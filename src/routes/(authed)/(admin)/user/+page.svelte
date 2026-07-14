d9552561c41cdc207f32d02d02e61ec919472d4b
Muhammad Sirojuddin <nidu672@gmail.com>
2026-07-14 21:01:27 +0700

    refactor(user): new user interface

--- src/routes/(authed)/(admin)/user/+page.svelte
+++ src/routes/(authed)/(admin)/user/+page.svelte
@@ -1,205 +1,4 @@
-<!-- <script lang="ts">
-	import { enhance, applyAction } from '$app/forms';
-	import { invalidateAll } from '$app/navigation';
-	import type { PageServerData } from './$types';
-	import { toast } from 'svelte-sonner';
-	type User = PageServerData['userList'][number];
-
-	let { data }: { data: PageServerData } = $props();
-
-	const users = $derived({
-		admin: data.userList.filter((a) => a.role === 'admin'),
-		santri: data.userList.filter((s) => s.role === 'santri'),
-		guru: data.userList.filter((g) => g.role === 'guru')
-	});
-	let deleteUserModal: HTMLDialogElement;
-	let editUserForm: HTMLFormElement;
-	let editUserModal: HTMLDialogElement;
-	let userToDelete = $state<string | null>(null);
-	let userToEdit = $state<User | null>(null);
-	let tambah: HTMLDialogElement;
-	let role = $state();
-	let listUserToDelete = $state<string[]>([]);
-</script>
-
- -->
-
-<!--Modal Hapus Data masih belum benar-->
-<!-- <dialog
-	class="modal"
-	id="delete_user_modal"
-	bind:this={deleteUserModal}
-	onclose={() => (userToDelete = null)}
->
-	<div class="modal-box">
-		<p>Apakah Anda yakin ingin menghapus data user ini?</p>
-		<div class="modal-action">
-			<form method="dialog">
-				<button class="btn btn-success" onclick={() => (userToDelete = null)}>Batal</button>
-			</form>
-			<form
-				action="?/hapus"
-				method="post"
-				use:enhance={() => {
-					return async ({ result }) => {
-						if (result.type === 'success') {
-							invalidateAll();
-							deleteUserModal.close();
-							if (result.data?.message && typeof result.data?.message === 'string') {
-								toast.success(result.data?.message);
-							}
-						} else if (result.type === 'failure') {
-							deleteUserModal.close();
-							if (result.data?.message && typeof result.data.message === 'string') {
-								toast.error(result.data?.message);
-							}
-						}
-						await applyAction(result);
-					};
-				}}
-			>
-				<input type="hidden" name="id" value={userToDelete} />
-				<button type="submit" class="btn btn-error">Delete</button>
-			</form>
-		</div>
-	</div>
-</dialog>
-<dialog class="modal" id="edit_user_modal" bind:this={editUserModal}>
-	<div class="modal-box">
-		<form method="dialog">
-			<button
-				class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-				onclick={() => (userToEdit = null)}>✕</button
-			>
-		</form>
-		<form
-			action="?/edit"
-			method="post"
-			bind:this={editUserForm}
-			use:enhance={() => {
-				return async ({ result }) => {
-					if (result.type === 'success') {
-						editUserForm.reset();
-						invalidateAll();
-						editUserModal.close();
-						if (result.data?.message && typeof result.data?.message === 'string') {
-							toast.success(result.data?.message);
-						}
-					} else if (result.type === 'failure') {
-						editUserModal.close();
-						if (result.data?.message && typeof result.data.message === 'string') {
-							toast.error(result.data?.message);
-						}
-					}
-					await applyAction(result);
-				};
-			}}
-		>
-			<fieldset class="fieldset">
-				<input type="text" name="id" id="id" value={userToEdit?.id} hidden />
-				<label for="edit_username" class="text-base label"> Username</label>
-				<input
-					type="text"
-					name="username"
-					id="edit_username"
-					class="input w-full"
-					value={userToEdit?.username}
-				/>
-				<label for="edit_nama" class="text-base label"> Nama </label>
-				<input
-					type="text"
-					name="nama"
-					id="edit_nama"
-					class="input w-full"
-					value={userToEdit?.nama}
-				/>
-				<label for="edit_password" class="text-base label">Password</label>
-				<input type="password" name="password" id="edit_password" class="input w-full" />
-				<label for="edit_role" class="text-base label"> Role</label>
-				<select name="role" id="edit_role" class="select w-full" disabled>
-					<option value="">Role</option>
-					<option value="admin" selected={userToEdit?.role == 'admin'}>Admin</option>
-					<option value="guru" selected={userToEdit?.role == 'guru'}>Guru</option>
-					<option value="santri" selected={userToEdit?.role == 'santri'}>Santri</option>
-				</select>
-				<button type="submit" class="btn btn-success mt-4">Edit Data</button>
-			</fieldset>
-		</form>
-	</div>
-</dialog>
-<dialog class="modal" id="tambah" bind:this={tambah}>
-	<div class="modal-box w-11/12 max-w-5xl">
-		<form method="dialog">
-			<button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-		</form>
-		<h2 class="card-title">Tambah User</h2>
-		<form action="?/tambah" method="post" class="flex flex-col gap-4">
-			<label for="role" class="label">Role</label>
-			<select name="role" id="role" bind:value={role} class="select w-full">
-				<option value="">Pilih</option>
-				<option value="admin">Admin</option>
-				<option value="guru">Guru</option>
-				<option value="santri">Santri</option>
-			</select>
-			{#if role === 'santri'}
-				<fieldset class="fieldset">
-					<legend class="fieldset-legend"> Nama Santri </legend>
-					<textarea
-						name="nama"
-						id="nama"
-						class="textarea w-full h-24"
-						placeholder="Masukkan satu Nama untuk satu baris. Contoh: 
-Abdullah Zaid 
-Ahmad Umar"
-					></textarea>
-					<label for="tipe" class="label">Pondok</label>
-					<select name="tipe" id="tipe" class="select w-full">
-						<option value=""></option>
-						<option value="putra">Putra</option>
-						<option value="putri">Putri</option>
-					</select>
-					<label for="tahun" class="label">Tahun Masuk</label>
-					<input type="number" name="tahun" id="" class="input w-full" placeholder="contoh: 2025" />
-				</fieldset>
-				<button type="submit" class="btn btn-success">Kirim</button>
-			{:else if role === 'guru'}
-				<fieldset class="fieldset">
-					<legend class="fieldset-legend"> Nama Guru </legend>
-					<textarea
-						name="nama"
-						id="nama"
-						class="textarea w-full h-24"
-						placeholder="Masukkan satu Nama untuk satu baris. Contoh:
-Abdul Aziz
-Amirul Hasan"
-					></textarea>
-				</fieldset>
-				<button type="submit" class="btn btn-success">Kirim</button>
-			{:else if role === 'admin'}
-				<label for="username" class="text-base label"> Username</label>
-				<input
-					name="username"
-					type="string"
-					id="username"
-					placeholder="Username"
-					class="input w-full"
-				/>
-				<label for="password" class="text-base label"> Password </label>
-				<input
-					type="password"
-					name="password"
-					id="password"
-					placeholder="Password"
-					class="input w-full"
-				/>
-				<label for="nama" class="text-base label"> Nama </label>
-				<input type="text" name="nama" id="nama" placeholder="Nama" class="input w-full" />
-				<button type="submit" class="btn btn-success">Kirim</button>
-			{/if}
-		</form>
-	</div>
-</dialog>
-<div class="flex flex-row-reverse">
+<!--<div class="flex flex-row-reverse">
 	<button class="btn btn-success" onclick={() => tambah.showModal()}>Tambah </button>
 </div>
 
@@ -351,16 +150,225 @@ Amirul Hasan"
 	function resetPagination() {
 		currentPage = 1;
 	}
+	let role = $state();
+	let tambah: HTMLDialogElement;
 </script>
+
+<!-- <script lang="ts">
+	import { enhance, applyAction } from '$app/forms';
+	import { invalidateAll } from '$app/navigation';
+	import type { PageServerData } from './$types';
+	import { toast } from 'svelte-sonner';
+	type User = PageServerData['userList'][number];
+
+	let { data }: { data: PageServerData } = $props();
+
+	const users = $derived({
+		admin: data.userList.filter((a) => a.role === 'admin'),
+		santri: data.userList.filter((s) => s.role === 'santri'),
+		guru: data.userList.filter((g) => g.role === 'guru')
+	});
+	let deleteUserModal: HTMLDialogElement;
+	let editUserForm: HTMLFormElement;
+	let editUserModal: HTMLDialogElement;
+	let userToDelete = $state<string | null>(null);
+	let userToEdit = $state<User | null>(null);
+	let tambah: HTMLDialogElement;
+	let role = $state();
+	let listUserToDelete = $state<string[]>([]);
+</script>
+
+ -->
+
+<!--Modal Hapus Data masih belum benar-->
+<!-- <dialog
+	class="modal"
+	id="delete_user_modal"
+	bind:this={deleteUserModal}
+	onclose={() => (userToDelete = null)}
+>
+	<div class="modal-box">
+		<p>Apakah Anda yakin ingin menghapus data user ini?</p>
+		<div class="modal-action">
+			<form method="dialog">
+				<button class="btn btn-success" onclick={() => (userToDelete = null)}>Batal</button>
+			</form>
+			<form
+				action="?/hapus"
+				method="post"
+				use:enhance={() => {
+					return async ({ result }) => {
+						if (result.type === 'success') {
+							invalidateAll();
+							deleteUserModal.close();
+							if (result.data?.message && typeof result.data?.message === 'string') {
+								toast.success(result.data?.message);
+							}
+						} else if (result.type === 'failure') {
+							deleteUserModal.close();
+							if (result.data?.message && typeof result.data.message === 'string') {
+								toast.error(result.data?.message);
+							}
+						}
+						await applyAction(result);
+					};
+				}}
+			>
+				<input type="hidden" name="id" value={userToDelete} />
+				<button type="submit" class="btn btn-error">Delete</button>
+			</form>
+		</div>
+	</div>
+</dialog>
+<dialog class="modal" id="edit_user_modal" bind:this={editUserModal}>
+	<div class="modal-box">
+		<form method="dialog">
+			<button
+				class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+				onclick={() => (userToEdit = null)}>✕</button
+			>
+		</form>
+		<form
+			action="?/edit"
+			method="post"
+			bind:this={editUserForm}
+			use:enhance={() => {
+				return async ({ result }) => {
+					if (result.type === 'success') {
+						editUserForm.reset();
+						invalidateAll();
+						editUserModal.close();
+						if (result.data?.message && typeof result.data?.message === 'string') {
+							toast.success(result.data?.message);
+						}
+					} else if (result.type === 'failure') {
+						editUserModal.close();
+						if (result.data?.message && typeof result.data.message === 'string') {
+							toast.error(result.data?.message);
+						}
+					}
+					await applyAction(result);
+				};
+			}}
+		>
+			<fieldset class="fieldset">
+				<input type="text" name="id" id="id" value={userToEdit?.id} hidden />
+				<label for="edit_username" class="text-base label"> Username</label>
+				<input
+					type="text"
+					name="username"
+					id="edit_username"
+					class="input w-full"
+					value={userToEdit?.username}
+				/>
+				<label for="edit_nama" class="text-base label"> Nama </label>
+				<input
+					type="text"
+					name="nama"
+					id="edit_nama"
+					class="input w-full"
+					value={userToEdit?.nama}
+				/>
+				<label for="edit_password" class="text-base label">Password</label>
+				<input type="password" name="password" id="edit_password" class="input w-full" />
+				<label for="edit_role" class="text-base label"> Role</label>
+				<select name="role" id="edit_role" class="select w-full" disabled>
+					<option value="">Role</option>
+					<option value="admin" selected={userToEdit?.role == 'admin'}>Admin</option>
+					<option value="guru" selected={userToEdit?.role == 'guru'}>Guru</option>
+					<option value="santri" selected={userToEdit?.role == 'santri'}>Santri</option>
+				</select>
+				<button type="submit" class="btn btn-success mt-4">Edit Data</button>
+			</fieldset>
+		</form>
+	</div>
+</dialog> -->
+<dialog class="modal" id="tambah" bind:this={tambah}>
+	<div class="modal-box w-11/12 max-w-5xl">
+		<form method="dialog">
+			<button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+		</form>
+		<h2 class="card-title">Tambah User</h2>
+		<form action="?/tambah" method="post" class="flex flex-col gap-4">
+			<label for="role" class="label">Role</label>
+			<select name="role" id="role" bind:value={role} class="select w-full">
+				<option value="">Pilih</option>
+				<option value="admin">Admin</option>
+				<option value="guru">Guru</option>
+				<option value="santri">Santri</option>
+			</select>
+			{#if role === 'santri'}
+				<fieldset class="fieldset">
+					<legend class="fieldset-legend"> Nama Santri </legend>
+					<textarea
+						name="nama"
+						id="nama"
+						class="textarea w-full h-24"
+						placeholder="Masukkan satu Nama untuk satu baris. Contoh: 
+Abdullah Zaid 
+Ahmad Umar"
+					></textarea>
+					<label for="tipe" class="label">Pondok</label>
+					<select name="tipe" id="tipe" class="select w-full">
+						<option value=""></option>
+						<option value="putra">Putra</option>
+						<option value="putri">Putri</option>
+					</select>
+					<label for="tahun" class="label">Tahun Masuk</label>
+					<input type="number" name="tahun" id="" class="input w-full" placeholder="contoh: 2025" />
+				</fieldset>
+				<button type="submit" class="btn btn-success">Kirim</button>
+			{:else if role === 'guru'}
+				<fieldset class="fieldset">
+					<legend class="fieldset-legend"> Nama Guru </legend>
+					<textarea
+						name="nama"
+						id="nama"
+						class="textarea w-full h-24"
+						placeholder="Masukkan satu Nama untuk satu baris. Contoh:
+Abdul Aziz
+Amirul Hasan"
+					></textarea>
+				</fieldset>
+				<button type="submit" class="btn btn-success">Kirim</button>
+			{:else if role === 'admin'}
+				<label for="username" class="text-base label"> Username</label>
+				<input
+					name="username"
+					type="string"
+					id="username"
+					placeholder="Username"
+					class="input w-full"
+				/>
+				<label for="password" class="text-base label"> Password </label>
+				<input
+					type="password"
+					name="password"
+					id="password"
+					placeholder="Password"
+					class="input w-full"
+				/>
+				<label for="nama" class="text-base label"> Nama </label>
+				<input type="text" name="nama" id="nama" placeholder="Nama" class="input w-full" />
+				<button type="submit" class="btn btn-success">Kirim</button>
+			{/if}
+		</form>
+	</div>
+</dialog>
 
 <svelte:head>
 	<title>Menu User</title>
 </svelte:head>
 
 <div class="p-6 min-h-screen">
-	<div class="mb-8">
-		<h1 class="text-3xl font-bold text-base-content">Daftar Pengguna</h1>
-		<p class="text-base-content/70 mt-1">Kelola data admin, guru, dan santri di sistem.</p>
+	<div class="flex flex-col md:flex-row justify-between items-start md:items-center mb-8 gap-4">
+		<div class="mb-8">
+			<h1 class="text-3xl font-bold text-base-content">Daftar Pengguna</h1>
+			<p class="text-base-content/70 mt-1">Kelola data admin, guru, dan santri di sistem.</p>
+		</div>
+		<button class="btn btn-primary" onclick={() => tambah.showModal()}
+			>Tambah Data User Baru
+		</button>
 	</div>
 
 	<div class="card bg-base-100 shadow-xl card-border">
